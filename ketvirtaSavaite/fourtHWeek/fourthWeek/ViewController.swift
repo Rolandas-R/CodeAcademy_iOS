ecd16e910c329ec4baacf1ec773d39800b108805
Rolandas-R <driveris@gmail.com>
2022-10-13 20:35:38 +0300

    autolayout pradzia

---
+++ ketvirtaSavaite/fourtHWeek/fourthWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fourtHWeek
+//
+//  Created by reromac on 2022-10-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
