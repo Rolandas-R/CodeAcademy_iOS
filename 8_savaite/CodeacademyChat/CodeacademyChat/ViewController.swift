206158cca0eaf3b8470dde0a80c4dbe495dca33c
Rolandas-R <driveris@gmail.com>
2022-11-08 21:31:27 +0200

    CodeAcademy Chat Init
    Init ir HomeView

---
+++ 8_savaite/CodeacademyChat/CodeacademyChat/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CodeacademyChat
+//
+//  Created by reromac on 2022-11-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "home" {
+//            if let viewController = segue.destination as? HomeViewController {
+//
+//            }
+//        }
+//    }
+//
+//
+//     .case login:
+//
+//     if let user = result.user {
+//         performSegue(withIdentifier: "home", sender: self)
+//
+//
+//
+}
+
