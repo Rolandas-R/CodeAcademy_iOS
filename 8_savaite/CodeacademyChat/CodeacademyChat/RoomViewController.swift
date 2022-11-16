f278478c49ed3c3e21167032ede3a6ac7c919f2a
Rolandas-R <driveris@gmail.com>
2022-11-16 19:32:45 +0200

    chato appsas kaip ir baigtas

--- 8_savaite/CodeacademyChat/CodeacademyChat/RoomViewController.swift
+++ 8_savaite/CodeacademyChat/CodeacademyChat/RoomViewController.swift
@@ -24,7 +24,6 @@ class RoomViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,20 +32,45 @@ class RoomViewController: UIViewController {
 
         welcomeLabel.text = "Welcome to the room: \(room!.name)"
     }
+    // MARK: - Private
+    
+    private func updateMessageTextView() {
+        //vienas is variantu
+        var messagesContent = [String]()
+//        var joinedString = ""
+        
+        for message in room.message {
+            let content =
+// variantas:    let joinedString +=
+//            if room.message.last?.date != message.date {
+//                joinedString += "--------------" + "\n"
+//            } else {
+//                joinedString += "\n"
+//            }
 
-    @IBAction func sendMessageButton(_ sender: Any) {
+            """
+            Date: \(message.date)
+            
+            User: \(message.username)
+            
+            Message: \(message.content)
+            """
+            
+        messagesContent.append(content)
+//            joinedString += "\n\n"
+        }
+    messageTextView.text = messagesContent.joined(separator: "\n------------ \n\n")
+        
+//        messageTextView.text = joinedString
+    }
+    
+    // MARK: - Action
+
+    @IBAction private func sendMessageButton(_ sender: Any) {
         room.writeMessage(messageContent: messageTextField.text!, sender: currentUser)
+        updateMessageTextView()
     }
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
